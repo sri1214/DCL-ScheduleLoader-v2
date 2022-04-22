568cc037bc4bad00c314c63735e8c45fb44de85b
Srikanth Vinukonda <srikanthchandu.v@gmail.com>
2022-04-22 06:38:18 -0500

    init

---
+++ src/main/java/com/dallascricket/scheduleloader/DclScheduleLoaderV2Application.java
@@ -0,0 +1,13 @@
+package com.dallascricket.scheduleloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclScheduleLoaderV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DclScheduleLoaderV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dallascricket/scheduleloader/DclScheduleLoaderV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dallascricket.scheduleloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclScheduleLoaderV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
